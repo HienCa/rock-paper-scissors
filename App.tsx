b30e324e550c6c12065d1f067951ed350fa8ea00
Van Hien Nguyen <hiennv@overate-vntech.com>
2024-10-16 09:27:06 +0700

    .

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { RockPaperScissors } from 'screens/rock-paper-scissors/RockPaperScissors';
-
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className='flex-1 justify-center items-center bg-white'>
       <StatusBar style="auto" />
       <RockPaperScissors />
+      <Text className="text-2xl">Open up App.tsx to start working on your app!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
